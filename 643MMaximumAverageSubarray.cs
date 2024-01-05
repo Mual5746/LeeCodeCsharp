f7bf35ca78731e065453708faf32aec3d6156dd6
DevOpsBoi <72858846+Mual5746@users.noreply.github.com>
2024-01-05 23:57:41 +0100

    Create 643MMaximumAverageSubarray.cs

---
+++ 643MMaximumAverageSubarray.cs
@@ -0,0 +1,9 @@
+//643 Maximum Average Subarray I
+
+public class Solution {
+    public double FindMaxAverage(int[] nums, int k) {
+        double avreagge = (double) nums.Sum()/k; 
+         
+         return avreagge;
+    }
+}
